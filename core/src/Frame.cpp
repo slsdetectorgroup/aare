e04d1e28f9c1c877f4582ed5574d3672f332c160
Bechir Braham <bachbrahem@gmail.com>
2024-03-20 16:27:22 +0100

    add dataspan

--- core/src/Frame.cpp
+++ core/src/Frame.cpp
@@ -1,6 +1,6 @@
 #include "aare/Frame.hpp"
 #include <iostream>
-
+#include <cassert>
 Frame::Frame(std::byte* bytes, ssize_t rows, ssize_t cols, ssize_t bitdepth):
   m_rows(rows), m_cols(cols), m_bitdepth(bitdepth) {
     m_data = new std::byte[rows*cols*bitdepth/8];
@@ -21,6 +21,19 @@ std::byte* Frame::get(int row, int col) {
   }
   return m_data+(row*m_cols + col)*(m_bitdepth/8);
 }
+
+template <typename T>
+void Frame::set(int row, int col, T data) {
+  assert(sizeof(T) == m_bitdepth/8);
+  if (row < 0 || row >= m_rows || col < 0 || col >= m_cols) {
+    std::cerr << "Invalid row or column index" << std::endl;
+    return;
+  }
+  std::memcpy(m_data+(row*m_cols + col)*(m_bitdepth/8), &data, m_bitdepth/8);
+}
+
+template void Frame::set(int row, int col, uint16_t data);
+
 
 // std::vector<std::vector<DataType>> Frame<DataType>::get_array() {
 //   std::vector<std::vector<DataType>> array;
---
+++ core/test/wrappers.test.cpp
@@ -0,0 +1,49 @@
+#include <aare/DataSpan.hpp>
+#include <aare/Frame.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <cstdint>
+
+TEST_CASE("Frame") {
+    auto data = new uint16_t[100];
+    for (int i = 0; i < 100; i++) {
+        data[i] = i;
+    }
+    Frame f(reinterpret_cast<std::byte *>(data), 10, 10, 16);
+    for (int i = 0; i < 100; i++) {
+        REQUIRE((uint16_t)*f.get(i / 10, i % 10) == data[i]);
+    }
+    REQUIRE(f.rows() == 10);
+    REQUIRE(f.cols() == 10);
+    REQUIRE(f.bitdepth() == 16);
+
+    uint16_t i = 44;
+    f.set(0, 0, i);
+    REQUIRE((uint16_t)*f.get(0, 0) == i);
+    delete[] data;
+}
+
+TEST_CASE("DataSpan") {
+    auto data = new uint16_t[100];
+    for (int i = 0; i < 100; i++) {
+        data[i] = i;
+    }
+    SECTION("constructors") {
+        DataSpan<uint16_t, 2> ds(data, std::vector<ssize_t>({10, 10}));
+        for (int i = 0; i < 100; i++) {
+            REQUIRE(ds(i / 10, i % 10) == data[i]);
+        }
+    }
+    SECTION("from Frame") {
+        Frame f(reinterpret_cast<std::byte *>(data), 10, 10, 16);
+        DataSpan<uint16_t, 2> ds(f);
+        for (int i = 0; i < 100; i++) {
+            REQUIRE(ds(i / 10, i % 10) == data[i]);
+        }
+
+        f.set(0, 0, (uint16_t)44);
+        REQUIRE((uint16_t)*f.get(0, 0) == 44); // check that set worked
+        REQUIRE(ds(0, 0) == 44);// check that ds is updated
+        REQUIRE(data[0] == 0); // check that data is not updated
+    }
+    delete[] data;
+}
