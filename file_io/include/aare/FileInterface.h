cffe020a6fbbdbe2a716cda9ce2e9920083da530
Erik Fröjdh <erik.frojdh@gmail.com>
2024-03-22 10:26:15 +0100

    added a draft for file interface

---
+++ file_io/include/aare/FileInterface.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "aare/Frame.hpp"
+#include <filesystem>
+namespace aare {
+class FileInterface {
+public:
+    FileInterface(std::filesystem::path& fname ){}; //do we need this? 
+    
+    // read the frame number on position frame_index
+    virtual size_t frame_number(size_t frame_index) = 0; 
+
+    // read one frame
+    virtual Frame read() = 0;
+
+    // read n_frames frames
+    virtual std::vector<Frame> read(size_t n_frames) = 0; //Is this the right interface?
+
+    // read one frame into the provided buffer
+    virtual void read_into(std::byte* image_buf) = 0;    
+
+    // read n_frames frame into the provided buffer
+    virtual void read_into(std::byte* image_buf, size_t n_frames) = 0; 
+    
+    // goto frame number
+    virtual void seek(size_t frame_number) = 0;
+
+    // return the position of the file pointer (in number of frames)
+    virtual size_t tell() = 0;
+
+    // total number of frames in the file
+    virtual size_t total_frames() = 0;
+
+    virtual ~FileInterface() = 0;
+};
+} // namespace aare
