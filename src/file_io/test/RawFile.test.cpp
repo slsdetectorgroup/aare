f13955672fea173e026b4b4df568c63ac21f0c9b
bechir.braham@psi.ch <bechir.braham@psi.ch>
2024-06-21 16:08:44 +0200

    fix last case?

--- src/file_io/test/RawFile.test.cpp
+++ src/file_io/test/RawFile.test.cpp
@@ -110,5 +110,5 @@ TEST_CASE("Read file with unordered frames") {
     auto fpath = test_data_path() / "mythen" / "scan242_master_3.raw";
     REQUIRE(std::filesystem::exists(fpath));
     File f(fpath, "r");
-    REQUIRE_THROWS(f.read());
+    REQUIRE_THROWS((f.read()));
 }
