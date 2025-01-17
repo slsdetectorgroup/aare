4f9f321e18403cd2b3e869b217456195a8637322
JulianHeymes <julian.heymes@psi.ch>
2025-01-17 16:38:07 +0100

    Added fit_gaus2 which takes dy as a 3rd parameters, and returns an array of length 6 [A0, mu, sigma, A0 error, muerror, sigma error]

--- include/aare/Fit.hpp
+++ include/aare/Fit.hpp
@@ -14,5 +14,7 @@ double gauss(const double x, const double *par);
 
 NDArray<double, 3> fit_gaus(NDView<double, 3> data, NDView<double, 1> x);
 NDArray<double, 1> fit_gaus(NDView<double, 1> data, NDView<double, 1> x);
+NDArray<double, 3> fit_gaus2(NDView<double, 3> data, NDView<double, 1> x, NDView<double, 3> data_err);
+NDArray<double, 1> fit_gaus2(NDView<double, 1> data, NDView<double, 1> x, NDView<double, 1> data_err);
 
 } // namespace aare
