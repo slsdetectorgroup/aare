37b14ef62eb5400367b166890739a169f8373665
Patrick <patrick.sieberer@psi.ch>
2025-02-04 17:36:58 +0100

    correct bit order

--- src/decode.cpp
+++ src/decode.cpp
@@ -6,18 +6,18 @@ uint16_t decode_adc(uint64_t input){
 
     //we want bits 29,19,28,18,31,21,27,20,24,23,25,22 and then pad to 16
     uint16_t output = 0;
-    output |= ((input >> 29) & 1) << 11;
-    output |= ((input >> 19) & 1) << 10;
-    output |= ((input >> 28) & 1) << 9;
-    output |= ((input >> 18) & 1) << 8;
-    output |= ((input >> 31) & 1) << 7;
-    output |= ((input >> 21) & 1) << 6;
-    output |= ((input >> 27) & 1) << 5;
-    output |= ((input >> 20) & 1) << 4;
-    output |= ((input >> 24) & 1) << 3;
-    output |= ((input >> 23) & 1) << 2;
-    output |= ((input >> 25) & 1) << 1;
-    output |= ((input >> 22) & 1) << 0;
+    output |= ((input >> 22) & 1) << 11;
+    output |= ((input >> 25) & 1) << 10;
+    output |= ((input >> 23) & 1) << 9;
+    output |= ((input >> 24) & 1) << 8;
+    output |= ((input >> 20) & 1) << 7;
+    output |= ((input >> 27) & 1) << 6;
+    output |= ((input >> 21) & 1) << 5;
+    output |= ((input >> 31) & 1) << 4;
+    output |= ((input >> 18) & 1) << 3;
+    output |= ((input >> 28) & 1) << 2;
+    output |= ((input >> 19) & 1) << 1;
+    output |= ((input >> 29) & 1) << 0;
     return output;
 }
 
