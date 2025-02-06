6eeb4a48cd27857858278951eeb2f57a8547fefd
froejdh_e <erik.frojdh@psi.ch>
2025-02-06 15:01:53 +0100

    WIP

--- src/RawFile.cpp
+++ src/RawFile.cpp
@@ -412,4 +412,61 @@ RawFile::~RawFile() {
     }
 }
 
+// //Update the geometry with a new ROI
+// //Do we need a new abstraction for this?
+// std::vector<aare::ModuleGeometry> geometry_from_roi(std::vector<aare::ModuleGeometry> m_module_pixel_0, aare::ROI roi) {
+
+//     int pos_y = 0;
+//     int pos_y_increment = 0;
+//     for (size_t row = 0; row < m_master.geometry().row; row++) {
+//         int pos_x = 0;
+//         for (size_t col = 0; col < m_master.geometry().col; col++) {
+//             auto &m = m_module_pixel_0[row * m_master.geometry().col + col];
+//             auto original_height = m.height;
+//             auto original_width = m.width;
+
+//             // module is to the left of the roi
+//             if (m.x + m.width < roi.xmin) {
+//                 m.width = 0;
+
+//                 // roi is in module
+//             } else {
+//                 // here we only arrive when the roi is in or to the left of
+//                 // the module
+//                 if (roi.xmin > m.x) {
+//                     m.width -= roi.xmin - m.x;
+//                 }
+//                 if (roi.xmax < m.x + original_width) {
+//                     m.width -= m.x + original_width - roi.xmax;
+//                 }
+//                 m.x = pos_x;
+//                 pos_x += m.width;
+//             }
+
+//             if (m.y + m.height < roi.ymin) {
+//                 m.height = 0;
+//             } else {
+//                 if ((roi.ymin > m.y) && (roi.ymin < m.y + m.height)) {
+//                     m.height -= roi.ymin - m.y;
+
+//                 }
+//                 if (roi.ymax < m.y + original_height) {
+//                     m.height -= m.y + original_height - roi.ymax;
+//                 }
+//                 m.y = pos_y;
+//                 pos_y_increment = m.height;
+//             }
+//         }
+//         // increment pos_y
+//         pos_y += pos_y_increment;
+//     }
+
+//     m_rows = roi.height();
+//     m_cols = roi.width();
+
+// }
+
+
+
+
 } // namespace aare
