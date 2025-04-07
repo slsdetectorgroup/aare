10e4e10431884af9a9f9abdd9084fcebba54247f
froejdh_e <erik.frojdh@psi.ch>
2025-04-07 15:33:37 +0200

    function signature for push back

--- python/aare/__init__.py
+++ python/aare/__init__.py
@@ -2,14 +2,14 @@
 from . import _aare
 
 
-from ._aare import File, RawMasterFile, RawSubFile
-from ._aare import Pedestal_d, Pedestal_f, ClusterFinder, VarClusterFinder
+# from ._aare import File, RawMasterFile, RawSubFile
+# from ._aare import Pedestal_d, Pedestal_f, ClusterFinder, VarClusterFinder
 from ._aare import DetectorType
-from ._aare import ClusterFile
+from ._aare import ClusterFile_Cluster3x3i as ClusterFile
 from ._aare import hitmap
 from ._aare import ROI
 
-from ._aare import ClusterFinderMT, ClusterCollector, ClusterFileSink, ClusterVector_i
+# from ._aare import ClusterFinderMT, ClusterCollector, ClusterFileSink, ClusterVector_i
 
 from ._aare import fit_gaus, fit_pol1
 from ._aare import Interpolator
