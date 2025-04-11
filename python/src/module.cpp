a59e9656be7b68428f7af210d1e913e7906ac0ca
Erik Fröjdh <erik.frojdh@psi.ch>
2025-04-11 16:54:21 +0200

    Making RawSubFile usable from Python (#158)
    - Removed a printout left from debugging
    - return also header when reading
    - added read_n 
    - check for error in ifstream

--- python/src/module.cpp
+++ python/src/module.cpp
@@ -10,6 +10,7 @@
 #include "cluster_file.hpp"
 #include "fit.hpp"
 #include "interpolation.hpp"
+#include "raw_sub_file.hpp"
 
 #include "jungfrau_data_file.hpp"
 
@@ -22,6 +23,7 @@ namespace py = pybind11;
 PYBIND11_MODULE(_aare, m) {
     define_file_io_bindings(m);
     define_raw_file_io_bindings(m);
+    define_raw_sub_file_io_bindings(m);
     define_ctb_raw_file_io_bindings(m);
     define_raw_master_file_bindings(m);
     define_var_cluster_finder_bindings(m);
