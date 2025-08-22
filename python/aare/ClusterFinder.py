d908ad36367db1580dae390022b9e6a3a6afab8f
Alice <alice.mazzoleni@psi.ch>
2025-08-22 15:25:15 +0200

    removed option to give clustersize

--- python/aare/ClusterFinder.py
+++ python/aare/ClusterFinder.py
@@ -46,14 +46,11 @@ def ClusterFinderMT(image_size, cluster_size = (3,3), dtype=np.int32, n_sigma=5,
     return cls(image_size, n_sigma=n_sigma, capacity=capacity, n_threads=n_threads)
 
 
-def ClusterCollector(clusterfindermt, cluster_size = (3,3), dtype=np.int32): 
+def ClusterCollector(clusterfindermt, dtype=np.int32): 
     """ 
     Factory function to create a ClusterCollector object. Provides a cleaner syntax for 
     the templated ClusterCollector in C++.
     """
-
-    if cluster_size != clusterfindermt.cluster_size:
-        raise Warning(f"Cluster size mismatch: ClusterCollector size {cluster_size} does not match ClusterFinderMT size {clusterfindermt.cluster_size}, using {clusterfindermt.cluster_size} instead.")
     
     cls = _get_class("ClusterCollector", clusterfindermt.cluster_size, dtype)
     return cls(clusterfindermt)
