49c94baff23db37944a5f7f495fe1017c4ac0c05
Alice <alice.mazzoleni@psi.ch>
2025-10-27 15:45:21 +0100

    added zero for proper conditional CDF

--- src/Interpolator.cpp
+++ src/Interpolator.cpp
@@ -10,16 +10,21 @@ Interpolator::Interpolator(NDView<double, 3> etacube, NDView<double, 1> xbins,
                            NDView<double, 1> ybins, NDView<double, 1> ebins)
     : m_ietax(etacube), m_ietay(etacube), m_etabinsx(xbins), m_etabinsy(ybins),
       m_energy_bins(ebins) {
-    if (etacube.shape(0) != xbins.size() || etacube.shape(1) != ybins.size() ||
-        etacube.shape(2) != ebins.size()) {
+    if (etacube.shape(0) + 1 != xbins.size() ||
+        etacube.shape(1) + 1 != ybins.size() ||
+        etacube.shape(2) + 1 != ebins.size()) {
         throw std::invalid_argument(
             "The shape of the etacube does not match the shape of the bins");
     }
+    m_ietax = NDArray<double, 3>(
+        std::array<ssize_t, 3>{xbins.size(), ybins.size(), ebins.size()});
+    m_ietax(0, 0, 0) = 0.0;
 
     // Cumulative sum in the x direction
     for (ssize_t i = 1; i < m_ietax.shape(0); i++) {
         for (ssize_t j = 0; j < m_ietax.shape(1); j++) {
             for (ssize_t k = 0; k < m_ietax.shape(2); k++) {
+                m_ietax(i, j, k) = etacube(i - 1, j, k);
                 m_ietax(i, j, k) += m_ietax(i - 1, j, k);
             }
         }
@@ -36,10 +41,15 @@ Interpolator::Interpolator(NDView<double, 3> etacube, NDView<double, 1> xbins,
         }
     }
 
+    m_ietay = NDArray<double, 3>(
+        std::array<ssize_t, 3>{xbins.size(), ybins.size(), ebins.size()});
+    m_ietay(0, 0, 0) = 0.0;
+
     // Cumulative sum in the y direction
     for (ssize_t i = 0; i < m_ietay.shape(0); i++) {
         for (ssize_t j = 1; j < m_ietay.shape(1); j++) {
             for (ssize_t k = 0; k < m_ietay.shape(2); k++) {
+                m_ietax(i, j, k) = etacube(i - 1, j, k);
                 m_ietay(i, j, k) += m_ietay(i, j - 1, k);
             }
         }
