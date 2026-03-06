ac7523424cec363d78a83879f99b981057485415
vhinger <viktoria.hinger@psi.ch>
2026-03-06 18:33:50 +0100

    Draft core remapping definitions

---
+++ include/aare/RemapDefs.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "aare/InclusiveROI.hpp"
+
+#include <vector>
+
+namespace aare::remap::defs {
+
+// Orientation of the sensor with respect to the ASICs / the HDI
+// Normal means lower part of sensor (as in GDS) aligns with lower part of HDI
+enum class Rotation : int { Normal = 0, Inverse = 1 };
+
+struct Guardring {
+    int x;
+    int y;
+};
+
+struct BondShift {
+    int x = 0;
+    int y = 0;
+};
+
+// Define the native pixel grid of the sensor (as connected to ASIC)
+struct SensorPixelGeometry {
+    int cols;
+    int rows;
+    Guardring guardring;
+};
+
+// Define the strixel grid of the sensor
+struct SensorStrixelGeometry {
+    int multiplicity;
+    double pitch_um;
+};
+
+// Fully characterize a strixel group (contiguous area that will be remapped)
+struct SensorGroupConfig {
+    SensorPixelGeometry pixel;
+    SensorStrixelGeometry strixel;
+    InclusiveROI placement; // location of the group in the sensor coordinate
+                            // system (or reduced roi)
+};
+
+// Combine multiple strixel areas that make up a single sensor
+struct SensorConfig {
+    std::vector<SensorGroupConfig> group_configs;
+};
+
+// Define location and orientation of a sensor on the module
+struct SensorPlacement {
+    InclusiveROI placement_on_module; // locatuon of the sensor in full-module
+                                      // coordinate system (or reduced rx_roi)
+    Rotation rotation;
+};
+
+} // namespace aare::remap::defs
