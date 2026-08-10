cc789b590531ea974123b5f6ec89c0220b25b3ac
Erik Fröjdh <erik.frojdh@gmail.com>
2026-08-10 16:18:43 +0200

    pre

--- include/aare/utils/math_helpers.hpp
+++ include/aare/utils/math_helpers.hpp
@@ -2,15 +2,13 @@
 #include <cstddef>
 
 namespace aare {
-    
+
 /**
  * @brief Compute the ceiling of the integer division of n by d.
  * @param n The numerator.
  * @param d The denominator.
  * @return The ceiling of the integer division.
  */
-constexpr size_t ceil_div(size_t n, size_t d){
-    return n / d + (n % d != 0);
-}
+constexpr size_t ceil_div(size_t n, size_t d) { return n / d + (n % d != 0); }
 
 } // namespace aare
